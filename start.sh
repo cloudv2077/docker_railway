18e63d5a15358fd21883b63c693ef1bd15744b12
cloudv2077 <mmix.xia@gmail.com>
2025-03-15 09:00:41 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,12 +3,10 @@ pwd
 pwd
 pwd
 pwd
-
 apt install python3 -y
 apt install python3-pip -y
-pip3 install -U yt-dlp -y 
+pip3 install -U yt-dlp
 yt-dlp --get-url -f best "https://www.youtube.com/watch?v=pnGxjcbztH0"
-
 pwd
 pwd
 pwd
